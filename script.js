63c567ec0f3da8f870d41565743b49c5f2428971
Pavan Gollapalli <Pavan@Pavans-MacBook-Pro.local>
2015-08-08 19:29:49 -0400

    added new files

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+			var  mn = $(".nav");
+    		mns = "nav-scrolled";
+    		hdr = $('header').height();
+
+			$(window).scroll(function() {
+  			if( $(this).scrollTop() > hdr ) {
+   				mn.addClass(mns);
+  			} else {
+    			mn.removeClass(mns);
+  			}
+			});
+		});
