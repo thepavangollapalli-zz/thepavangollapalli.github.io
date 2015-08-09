e38b4343c3e9f92c770c1cee0070c958142c9761
Pavan Gollapalli <Pavan@Pavans-MacBook-Pro.local>
2015-08-09 10:22:11 -0400

    added js folder

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+			var  mn = $(".nav");
+    		mns = "nav-scrolled";
+    		hdr = $('header').height();
+
+			$(window).scroll(function() {
+  			if( $(this).scrollTop() > hdr ) {
+   				mn.addClass(mns);
+  			} else {
+    			mn.removeClass(mns);
+  			}
+			});
+
+      $('a[href^="#"]').on('click', function(event) {
+
+       var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        }
+
+      });
+		});
