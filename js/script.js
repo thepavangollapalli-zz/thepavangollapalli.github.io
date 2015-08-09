6bcf91f31a74656eae68e783e102ea8f2a7c6ed6
Pavan Gollapalli <Pavan@Pavans-MacBook-Pro.local>
2015-08-09 11:26:49 -0400

    fixed nav highlight bug

--- js/script.js
+++ js/script.js
@@ -29,4 +29,26 @@ $(document).ready(function() {
         $('nav li').removeClass('active');
         $("[href=#"+id+"]").closest('li').addClass('active');
       });
+
+      var main = main = $('nav ul');
+
+      $('nav a').click(function(event) {
+        
+      event.preventDefault();
+ 
+      var full_url = this.href,
+        parts = full_url.split('#'),
+        trgt = parts[1],
+        target_offset = $('#'+trgt).offset(),
+        target_top = target_offset.top;
+    
+        /* Remove active class on any li when an anchor is clicked */
+    
+      main.children().removeClass();
+    
+      /* Add active class to clicked anchor's parent li */
+        
+      $(this).parent().addClass('active');
+
+});
 });
